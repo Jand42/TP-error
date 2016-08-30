00d47aa152ee994334afd138e043b32fbfd473be
András Jankó <andras.janko@gmail.com>
2016-08-30 15:57:01 +0200

    initial commit

---
+++ TestTP/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace TestTP.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("TestTP")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("TestTP")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("d8832d85-e87a-4d67-a9f1-83b3c08ae278")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ TestTP/Library1.fs
@@ -0,0 +1,150 @@
+﻿namespace TestTP
+
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System.Reflection
+
+module Helper =
+    let doNothing() = ()
+    let doNothingOneArg(x:int) = ()
+    let doNothingGeneric(x:'T) = ()
+    type C() = 
+        static member DoNothing() = ()
+        static member DoNothingOneArg(x:int) = ()
+        static member DoNothingTwoArg(c:C, x:int) = ()
+        static member DoNothingGeneric(x:'T) = ()
+        member __.InstanceDoNothing() = ()
+        member __.InstanceDoNothingOneArg(x:int) = ()
+        member __.InstanceDoNothingTwoArg(c:C, x:int) = ()
+        member __.InstanceDoNothingGeneric(x:'T) = ()
+
+    type G<'U>() = 
+        static member DoNothing() = ()
+        static member DoNothingOneArg(x:int) = ()
+        static member DoNothingTwoArg(c:C, x:int) = ()
+        static member DoNothingGeneric(x:'T) = ()
+        member __.InstanceDoNothing() = ()
+        member __.InstanceDoNothingOneArg(x:int) = ()
+        member __.InstanceDoNothingTwoArg(c:C, x:int) = ()
+        member __.InstanceDoNothingGeneric(x:'U) = ()
+
+    type R = { A : int; mutable B : int }
+
+[<TypeProvider>]
+type BasicProvider (config : TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces ()
+
+    let ns = "ErasedWithConstructor.Provided"
+    let asm = Assembly.GetExecutingAssembly()
+
+    let createTypes () =
+        let myType = ProvidedTypeDefinition(asm, ns, "MyType", Some typeof<obj>)
+
+        let ctor = ProvidedConstructor([], InvokeCode = fun args -> <@@ "My internal state" :> obj @@>)
+        myType.AddMember(ctor)
+
+        let ctor2 = ProvidedConstructor(
+                        [ProvidedParameter("InnerState", typeof<string>)],
+                        InvokeCode = fun args -> <@@ (%%(args.[0]):string) :> obj @@>)
+        myType.AddMember(ctor2)
+
+        let innerState = ProvidedProperty("InnerState", typeof<string>,
+                            GetterCode = fun args -> <@@ (%%(args.[0]) :> obj) :?> string @@>)
+        myType.AddMember(innerState)
+
+        let someMethod = ProvidedMethod("DoNothing", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.doNothing() @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("DoNothingOneArg", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.doNothingOneArg(3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("DoNothingGeneric", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.doNothingGeneric(3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("ClassDoNothing", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.C.DoNothing() @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("ClassDoNothingGeneric", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.C.DoNothingGeneric(3) @@>)
+
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("ClassDoNothingOneArg", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.C.DoNothingOneArg(3) @@>)
+
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("ClassDoNothingTwoArg", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.C.DoNothingTwoArg(Helper.C(), 3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("ClassInstanceDoNothing", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.C().InstanceDoNothing() @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("ClassInstanceDoNothingGeneric", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.C().InstanceDoNothingGeneric(3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("ClassInstanceDoNothingOneArg", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.C().InstanceDoNothingOneArg(3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("ClassInstanceDoNothingTwoArg", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.C().InstanceDoNothingTwoArg(Helper.C(), 3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("GenericClassDoNothing", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.G<int>.DoNothing() @@>)
+        myType.AddMember(someMethod)
+
+        // These do not seem to compile correctly when used in provided expressions:
+        //Helper.G<int>.DoNothingGeneric(3)
+
+        // These do not seem to compile correctly when used in provided expressions:
+        //Helper.G<int>().InstanceDoNothingGeneric(3)
+                                                         
+        let someMethod = ProvidedMethod("GenericClassDoNothingOneArg", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.G<int>.DoNothingOneArg(3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("GenericClassDoNothingTwoArg", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.G<int>.DoNothingTwoArg(Helper.C(), 3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("GenericClassInstanceDoNothing", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.G<int>().InstanceDoNothing() @@>)
+        myType.AddMember(someMethod)
+
+
+        let someMethod = ProvidedMethod("GenericClassInstanceDoNothingOneArg", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.G<int>().InstanceDoNothingOneArg(3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("GenericClassInstanceDoNothingTwoArgs", [], typeof<unit>,
+                            InvokeCode = fun args -> <@@ Helper.G<int>().InstanceDoNothingTwoArg(Helper.C(), 3) @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("OptionConstructionAndMatch", [], typeof<int>,
+                            InvokeCode = fun args -> <@@ match Some 1 with None -> 0 | Some x -> x @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("ChoiceConstructionAndMatch", [], typeof<int>,
+                            InvokeCode = fun args -> <@@ match Choice1Of2 1 with Choice2Of2 _ -> 0 | Choice1Of2 x -> x @@>)
+        myType.AddMember(someMethod)
+
+        let someMethod = ProvidedMethod("RecordConstructionAndFieldGetSet", [], typeof<int>,
+                            InvokeCode = fun args -> <@@ let r : Helper.R = { A = 1; B = 0 } in r.B <- 1; r.A @@>)
+        myType.AddMember(someMethod)
+
+        [myType]
+
+    do
+        this.AddNamespace(ns, createTypes())
+
+[<assembly:TypeProviderAssembly>]
+do ()
